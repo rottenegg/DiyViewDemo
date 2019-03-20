6c5f064b3ae13f4a6e89a4f483eb7740e9cc438a
rottenegg <819694871@qq.com>
2019-03-20 16:46:33 +0800

    网格

---
+++ app/src/main/java/com/huangjinfu/mydiyviewdemo/views/TableView.java
@@ -0,0 +1,24 @@
+package com.huangjinfu.mydiyviewdemo.views;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by huangjinfu on 2019/3/20.
+ */
+
+public class TableView extends View{
+    public TableView(Context context) {
+        super(context);
+    }
+
+    public TableView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TableView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
